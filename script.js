8df30462f0253c02caf8533ca97ed538729ac0bd
Vishal Kumar Sain <102027742+vis123Git@users.noreply.github.com>
2024-07-26 09:53:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-function calculateMinCost() {
-  //your code here
+const res = document.getElementById("result")
+const inputs = document.getElementById("rope-lengths")
+
+function calculateMinCost(args) {
+	const values = inputs.value.split(",").map(Number).sort((a,b)=>a-b); 
+	for (let [index,key] of values.entries()) {
+		console.log("key, index===",key, index)
+	}
+
+	console.log("args===", inputs.value, values.length)
+  res.innerText = 25
   
   
   
